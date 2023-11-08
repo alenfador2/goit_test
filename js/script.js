835be22db807d52dc91f1e85ad560880e6ef4425
alenfador2 <129333585+alenfador2@users.noreply.github.com>
2023-11-08 23:57:20 +0100

    Update 1.0

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const score = document.getElementById('score');
+const button = document.querySelector('.clear');
+const cards = document.querySelectorAll('.card');
+const uncounted = document.querySelector('.uncounted');
+let points = 0;
+for(const card of cards){
+    card.addEventListener('click', (event) =>{
+        incrementScore();
+        pickedCard();
+        
+    });
+    function pickedCard(){
+        if(!card.classList.contains('visited')){
+            card.classList.add('visited');
+        }
+    }
+    function incrementScore(){
+        points++;
+        score.innerHTML = `Prawidłowa karta! Punkty: ${points}`;
+        console.log(points);
+    }
+    function handleUncounted(event){
+        const phase = event.eventPhase;
+        if(phase === event.CAPTURING_PHASE){
+            score.innerHTML = `Blokowana karta! Punkty: ${points}`;
+            event.stopPropagation();
+        }
+    }
+    uncounted.addEventListener('click', handleUncounted, { capture: true });
+    function clear() {
+        score.innerHTML = `Punkty: ${points = 0}`;
+        if (card.classList.contains('visited')){
+            card.classList.remove('visited');
+        };
+    }
+    button.addEventListener('click', clear);
+}
+
